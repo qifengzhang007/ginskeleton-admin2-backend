950c848dee03bb37c6f4114bbb8a33dbbe2758a6
飘渺 <17521302119>
2021-03-21 16:37:22 +0800

    用户登录完善登录完善

--- app/model/users/users.go
+++ app/model/users/users.go
@@ -19,16 +19,15 @@ func CreateUserFactory(sqlType string) *UsersModel {
 
 type UsersModel struct {
 	model.BaseModel
-	UserName         string `gorm:"column:user_name" json:"user_name"`
-	Pass             string `json:"pass"`
-	Phone            string `json:"phone"`
-	RealName         string `gorm:"column:real_name" json:"real_name"`
-	Status           int    `json:"status"`
-	FrProvinceCityId int    `gorm:"column:fr_province_city_id" json:"city_id"`
-	Avatar           string `gorm:"column:avatar" json:"avatar"`
-	LoginTimes       int    `json:"login_times"`
-	Remark           string `json:"remark"`
-	LastLoginIp      string `gorm:"column:last_login_ip" json:"last_login_ip"`
+	UserName    string `gorm:"column:user_name" json:"user_name"`
+	Pass        string `json:"pass"`
+	Phone       string `json:"phone"`
+	RealName    string `gorm:"column:real_name" json:"real_name"`
+	Status      int    `json:"status"`
+	Avatar      string `gorm:"column:avatar" json:"avatar"`
+	LoginTimes  int    `json:"login_times"`
+	Remark      string `json:"remark"`
+	LastLoginIp string `gorm:"column:last_login_ip" json:"last_login_ip"`
 }
 
 // 表名
@@ -49,12 +48,18 @@ func (u *UsersModel) Register(userName, pass, userIp string) bool {
 
 // 用户登录,
 func (u *UsersModel) Login(userName string, pass string) *UsersModel {
-	sql := "select id, user_name,real_name,pass,phone,fr_province_city_id  from tb_users where  user_name=?  limit 1"
+	sql := "select id, user_name,real_name,pass,phone  from tb_users where  user_name=?  limit 1"
 	result := u.Raw(sql, userName).First(u)
 	if result.Error == nil {
 		// 账号密码验证成功
 		if len(u.Pass) > 0 && (u.Pass == md5_encrypt.Base64Md5(pass)) {
-			return u
+			//同时将用户表登陆次数 ++
+			sql = "UPDATE  tb_users  set login_times=login_times+1  where  id=?"
+			if result = u.Exec(sql, u.Id); result.Error == nil {
+				return u
+			} else {
+				variable.ZapLog.Error("用户登录次数++出错:", zap.Error(result.Error))
+			}
 		}
 	} else {
 		variable.ZapLog.Error("根据账号查询单条记录出错:", zap.Error(result.Error))
