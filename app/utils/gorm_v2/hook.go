16d1cc3079065649c91a8f7548adcac598061bb6
张奇峰 <1990966837@qq.com>
2021-12-29 11:19:08 +0800

    更新： 1.gorm查询时,created_at、updated_at字段默认格式为 yyyy-mm-dd。 2.gorm批量新增时支持自动给切片的中的created_at、updated_at字段赋值。

--- app/utils/gorm_v2/hook.go
+++ app/utils/gorm_v2/hook.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-// 这里的函数都是gorm的hook函数，拦截一些官方我们认为不合格的操作行为，提升项目整体的完美性
+// 这里的函数都是gorm的hook函数，拦截一些官方认为标准提示，但是我们认为不合格的操作行为，提升项目整体的完美性
 
 // MaskNotDataError 解决gorm v2 包在查询无数据时，报错问题（record not found），但是官方认为报错是应该是，我们认为查询无数据，代码一切ok，不应该报错
 func MaskNotDataError(gormDB *gorm.DB) {
@@ -19,8 +19,21 @@ func MaskNotDataError(gormDB *gorm.DB) {
 // InterceptCreatePramsNotPtrError 拦截 create 函数参数如果是非指针类型的错误,新用户最容犯此错误
 
 func CreateBeforeHook(gormDB *gorm.DB) {
+	destValueOf := reflect.ValueOf(gormDB.Statement.Dest).Elem()
 	if reflect.TypeOf(gormDB.Statement.Dest).Kind() != reflect.Ptr {
 		_ = gormDB.AddError(errors.New(my_errors.ErrorsGormDBCreateParamsNotPtr))
+	} else if destValueOf.Type().Kind() == reflect.Slice || destValueOf.Type().Kind() == reflect.Array {
+		inLen := destValueOf.Len()
+		for i := 0; i < inLen; i++ {
+			row := destValueOf.Index(i)
+			if row.Type().Kind() == reflect.Struct {
+				row.FieldByName("CreatedAt").Set(reflect.ValueOf(time.Now().Format(variable.DateFormat)))
+				row.FieldByName("UpdatedAt").Set(reflect.ValueOf(time.Now().Format(variable.DateFormat)))
+			} else if row.Type().Kind() == reflect.Map {
+				row.SetMapIndex(reflect.ValueOf("created_at"), reflect.ValueOf(time.Now().Format(variable.DateFormat)))
+				row.SetMapIndex(reflect.ValueOf("updated_at"), reflect.ValueOf(time.Now().Format(variable.DateFormat)))
+			}
+		}
 	} else {
 		// 参数校验无错误自动设置 CreatedAt、 UpdatedAt
 		gormDB.Statement.SetColumn("created_at", time.Now().Format(variable.DateFormat))
@@ -29,7 +42,7 @@ func CreateBeforeHook(gormDB *gorm.DB) {
 }
 
 // InterceptUpdatePramsNotPtrError 拦截 save、update 函数参数如果是非指针类型的错误
-// 对于开发者来说，以结构体形式更新数，只需要在 update 、save 函数的参数前面添加 & 即可
+// 对于开发者来说，以结构体形式更新数据，只需要在 update 、save 函数的参数前面添加 & 即可
 // 最终就可以完美兼支持、兼容 gorm 的所有回调函数
 // 但是如果是指定字段更新，例如： UpdateColumn 函数则只传递值即可，不需要做校验
 
