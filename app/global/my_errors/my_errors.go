add3de74f1b7ba6248a580b1e5b5c9f9e03d7e47
张奇峰 <1990966837@qq.com>
2022-03-22 18:36:40 +0800

    1.更新gorm回调函数，增加一些逻辑判断，加强回调函数执行前条件检查的严谨性

--- app/global/my_errors/my_errors.go
+++ app/global/my_errors/my_errors.go
@@ -25,7 +25,7 @@ const (
 	// 数据库部分
 	ErrorsDbDriverNotExists        string = "数据库驱动类型不存在,目前支持的数据库类型：mysql、sqlserver、postgresql，您提交数据库类型："
 	ErrorsDialectorDbInitFail      string = "gorm dialector 初始化失败,dbType:"
-	ErrorsGormDBCreateParamsNotPtr string = "gorm Create 函数的参数必须是一个指针"
+	ErrorsGormDBCreateParamsNotPtr string = "gorm Create 函数的参数必须是一个指针, 为了完美支持 gorm 的所有回调函数,请在参数前面添加 & "
 	ErrorsGormDBUpdateParamsNotPtr string = "gorm 的 Update、Save 函数的参数必须是一个指针(GinSkeleton ≥ v1.5.29 版本新增验证，为了完美支持 gorm 的所有回调函数,请在参数前面添加 & )"
 
 	//redis部分
